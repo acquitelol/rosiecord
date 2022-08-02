e8c9338fa6115fe6896b2ab22d6ede97a76ba85e
devicarus <devicarus@icloud.com>
2022-08-02 16:08:45 +0200

    :ambulance: Fixed patching script

--- patch.sh
+++ patch.sh
@@ -5,7 +5,7 @@ mkdir dist
 for Pack in $(ls Packs) 
 do
     unzip Enmity.ipa
-    cp -rf Packs/${Pack}/ Payload/Discord.app/assets/
+    cp -rf Packs/${Pack}/* Payload/Discord.app/assets/
     zip -r dist/${Pack}cord.ipa Payload
     rm -rf Payload
 done
