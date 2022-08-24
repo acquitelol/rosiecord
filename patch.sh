e2325670cbc57fe66496b0f058abdf3aec19e2aa
devicarus <devicarus@icloud.com>
2022-08-24 19:30:35 +0200

    :alien: Updated to match the new filename

--- patch.sh
+++ patch.sh
@@ -4,7 +4,7 @@ mkdir dist
 
 for Pack in $(ls Packs) 
 do
-    unzip Enmity.ipa
+    unzip enmity.ipa
     cp -rf Packs/${Pack}/* Payload/Discord.app/assets/
     zip -r dist/${Pack}cord.ipa Payload
     rm -rf Payload
