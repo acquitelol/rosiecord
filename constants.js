70a49606fc365e116cc00d51b3be883d1dfb6c07
Acquite <acquitefx@gmail.com>
2023-04-01 15:53:20 +0100

    [173] build 42047

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 export default {
-    IPA_FETCH_LINK: "https://cdn.discordapp.com/attachments/1085420899907412028/1090194324307841076/Discord_173.0_41816.ipa",
+    IPA_FETCH_LINK: "https://cdn.discordapp.com/attachments/1085420899907412028/1091445913630802002/Discord_173.0_42047.ipa",
     ENMITY_LOADER: "Enmity.Debug.deb",
     GET_PATCH_TYPE(predicate, inputArg, truePredicate, falsePredicate, fallback) {
         const out = predicate(inputArg);
