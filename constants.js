bb38522fe42ec3e6b225f7194065e3bfb95ef6f5
Acquite <acquitefx@gmail.com>
2023-09-16 00:58:55 +0100

    [196] (!ActionSheets are broken on this version!)
    Plugins will need to fix their ActionSheet patches.

--- constants.js
+++ constants.js
@@ -58,5 +58,5 @@ class States extends Colors {
 }
 class Constants {
 }
-Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_195.0_49413.ipa";
+Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_196.0_49729.ipa";
 export { Shell, Colors, Divider, States, Constants };
--- constants.ts
+++ constants.ts
@@ -65,7 +65,7 @@ class States extends Colors {
 }
 
 class Constants {
-    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_195.0_49413.ipa";
+    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_196.0_49729.ipa";
 }
 
 export { Shell, Colors, Divider, States, Constants };
