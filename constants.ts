602a4725236689c43f56dfde1084c87b4efff4d4
Rosie <acquitefx@gmail.com>
2023-10-01 13:31:45 +0100

    Update IPA to 198

--- constants.ts
+++ constants.ts
@@ -65,7 +65,7 @@ class States extends Colors {
 }
 
 class Constants {
-    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_197.0_49832.ipa";
+    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_198.0_50360.ipa";
 }
 
 export { Shell, Colors, Divider, States, Constants };
