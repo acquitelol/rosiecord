e718ba0c48d43b29e13fcaea59c0762f2ffcf2cb
SerStars <90714930+SerStars@users.noreply.github.com>
2023-10-11 22:15:20 +0200

    Update Version to v200

--- constants.ts
+++ constants.ts
@@ -65,7 +65,7 @@ class States extends Colors {
 }
 
 class Constants {
-    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_198.0_50360.ipa";
+    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_200.0_50625.ipa";
 }
 
 export { Shell, Colors, Divider, States, Constants };
