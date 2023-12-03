acd67d88a6d7f2a6ed19e35b324be1bc6307518d
Rosie <acquitefx@gmail.com>
2023-12-03 17:35:45 +0000

    Update to 208, fixing chat input bug

--- constants.js
+++ constants.js
@@ -58,5 +58,5 @@ class States extends Colors {
 }
 class Constants {
 }
-Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_207.0_52588.ipa";
+Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_208.0_52741.ipa";
 export { Shell, Colors, Divider, States, Constants };
--- constants.ts
+++ constants.ts
@@ -65,7 +65,7 @@ class States extends Colors {
 }
 
 class Constants {
-    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_207.0_52588.ipa";
+    static IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_208.0_52741.ipa";
 }
 
 export { Shell, Colors, Divider, States, Constants };
