2e678459b670d332b00cbd56d842740204b90146
Rosie <acquitefx@gmail.com>
2023-12-11 23:05:12 +0000

    actually build for 209.0

--- constants.js
+++ constants.js
@@ -58,5 +58,5 @@ class States extends Colors {
 }
 class Constants {
 }
-Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_208.0_52874.ipa";
+Constants.IPA_FETCH_LINK = "https://ipa.aspy.dev/discord/testflight/Discord_209.0_53119.ipa";
 export { Shell, Colors, Divider, States, Constants };
